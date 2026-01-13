c35ac1a206add9a6c6d18271893be2b227e7d02c
Yevheniia Furutina <yevfur-ipt25@lll.kpi.ua>
2026-01-13 02:32:22 +0100

    added all needed includes, namespaces and int main() to classic MITM attack

---
+++ Project1/MITM_attack_classic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using namespace boost::multiprecision;
+
+int main(){
+
+
+}
