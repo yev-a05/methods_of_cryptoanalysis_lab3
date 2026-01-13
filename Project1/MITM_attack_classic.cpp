db612c209073d1bfb857e0bb5e92813feba41350
Yevheniia Furutina <yevfur-ipt25@lll.kpi.ua>
2026-01-13 02:48:17 +0100

    added functions extended_euclid_alg and gcd_for_numbers from the first attack

--- Project1/MITM_attack_classic.cpp
+++ Project1/MITM_attack_classic.cpp
@@ -10,6 +10,55 @@
 using namespace std;
 using namespace boost::multiprecision;
 
+vector<cpp_int> gcd_for_numbers(cpp_int N_i, cpp_int n_i) {
+
+	cpp_int r0 = N_i;
+	cpp_int r1 = n_i;
+	cpp_int r2 = 1;
+	vector<cpp_int> Q;
+
+	while (r2 > 0) {
+		cpp_int q = r0 / r1;
+		r2 = r0 % r1;
+		Q.push_back(q);
+		r0 = r1;
+		r1 = r2;
+	}
+	return Q;
+}
+
+cpp_int extended_euclid_alg(cpp_int N_i, cpp_int n_i) {
+
+	N_i = N_i % n_i;
+	vector<cpp_int> for_q = gcd_for_numbers(N_i, n_i);
+	cpp_int u0 = 1;
+	cpp_int u1 = 0;
+	cpp_int v1 = 1;
+	cpp_int v0 = 0;
+
+	for (long long i = 0; i < for_q.size() - 1; i++) {
+		cpp_int q = for_q[i];
+		cpp_int u2, v2;
+		u2 = u0 - u1 * q;
+		v2 = v0 - v1 * q;
+		u0 = u1;
+		u1 = u2;
+		v0 = v1;
+		v1 = v2;
+	}
+
+	cpp_int u = u1;
+	cpp_int v = v1;
+	cpp_int M = 0;
+
+	if (N_i * u + n_i * v == 1) {
+		if (u < 0) { u = u + n_i; }
+		M = u;
+	}
+
+	return M;
+}
+
 int main(){
 	int l = 20;
 	cpp_int e("65537");
@@ -19,5 +68,6 @@ int main(){
 
 	vector<pair<cpp_int, cpp_int>> X;
 	vector<cpp_int> Te_converse;
+
 
 }
